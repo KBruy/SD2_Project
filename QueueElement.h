e60308903d9cb348ef221bc864632966546bbf7d
KBury <krystian.bury1@gmail.com>
2026-05-02 13:46:48 +0200

    feat: initial priority quene project setup

---
+++ QueueElement.h
@@ -0,0 +1,11 @@
+#ifndef QUEUE_ELEMENT_H
+#define QUEUE_ELEMENT_H
+
+struct QueueElement {
+    int value;
+    int priority;
+
+};
+
+#endif
+
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Projekt 2 - Kolejka priorytetowa MAX" << std::endl;
+    return 0;
+}

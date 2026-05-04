a922d22bdeca4ef24b7be1b73a4d379b65724bd1
KBury <krystian.bury1@gmail.com>
2026-05-04 18:14:57 +0200

    feat: add researh data generation

--- Research.cpp
+++ Research.cpp
@@ -4,13 +4,13 @@
 #include <cstdlib>
 #include <ctime>
 
-const int SERIES_COUNT = 10;
-const int COPIES_COUNT = 10;
+// const int SERIES_COUNT = 10;
+// const int COPIES_COUNT = 10;
 const int BASE_SEED = 12345;
 const int MAX_VALUE = 16777215;
 
-const int SIZES_COUNT = 7;
-const int TEST_SIZES[SIZES_COUNT] = {5000, 10000, 25000, 50000, 100000, 250000, 500000};\
+//const int SIZES_COUNT = 7;
+//const int TEST_SIZES[SIZES_COUNT] = {5000, 10000, 25000, 50000, 100000, 250000, 500000};
 
 int Research::generateRandomNumber(int minValue, int maxValue) {
     return minValue + rand() % (maxValue - minValue + 1); //zakres losowania 0 - 16777215
@@ -28,5 +28,36 @@ void Research::runAll() {
 
     file.close();
     std::cout<<"Utworzono plik results.csv" <<std::endl;
+
+    int testSize = 10;
+int testSeed = BASE_SEED;
+
+QueueElement* data = new QueueElement[testSize];
+
+generateData(data, testSize, testSeed);
+
+// tymczasowy test - zapisujemy kilka danych do pliku
+for (int i = 0; i < testSize; i++) {
+    file << "TEST,generated,"
+         << testSize << ","
+         << 0 << ","
+         << testSeed << ","
+         << 0 << ","
+         << data[i].value << ","
+         << data[i].priority
+         << "\n";
 }
 
+delete[] data;
+}
+
+void Research::generateData(QueueElement* data, int size, int seed){
+    srand(seed);
+
+    int maxPriority = 10*size; //zakres kilkukrotnie wiekszy od rozmiaru struktury
+
+    for (int i = 0; i < size; i++) {
+        data[i].value = generateRandomNumber(0, MAX_VALUE);
+        data[i].priority = generateRandomNumber(0, maxPriority);
+    }
+}
--- Research.h
+++ Research.h
@@ -1,9 +1,13 @@
 #ifndef RESEARCH_H
 #define RESEARCH_H
 
+#include "QueueElement.h"
+
 class Research {
     private:
         int generateRandomNumber(int minValue, int maxValue);
+        
+        void generateData(QueueElement* data, int size, int seed); //przygotowywuje dane do badan
     public:
         void runAll(); //uruchamia wszystkie badania i zapisuje wyniki do csv
 

e81442c63ac50c6b4645208aa05141de00d942bb
KBury <krystian.bury1@gmail.com>
2026-05-04 18:33:04 +0200

    feat: add max heap insert measurement

--- Research.cpp
+++ Research.cpp
@@ -3,14 +3,17 @@
 #include "Research.h"
 #include <cstdlib>
 #include <ctime>
+#include <fstream>
+#include <chrono>
+#include "MaxHeapPriorityQueue.h"
 
-// const int SERIES_COUNT = 10;
-// const int COPIES_COUNT = 10;
+const int SERIES_COUNT = 10;
+const int COPIES_COUNT = 10;
 const int BASE_SEED = 12345;
 const int MAX_VALUE = 16777215;
 
-//const int SIZES_COUNT = 7;
-//const int TEST_SIZES[SIZES_COUNT] = {5000, 10000, 25000, 50000, 100000, 250000, 500000};
+const int SIZES_COUNT = 7;
+const int TEST_SIZES[SIZES_COUNT] = {5000, 10000, 25000, 50000, 100000, 250000, 500000};
 
 int Research::generateRandomNumber(int minValue, int maxValue) {
     return minValue + rand() % (maxValue - minValue + 1); //zakres losowania 0 - 16777215
@@ -26,29 +29,17 @@ void Research::runAll() {
 
     file << "structure,operation,size,series,seed,copies,total_time_ns,avg_time_ns\n";
 
+    for (int i = 0; i < SIZES_COUNT; i++){
+        int currentSize = TEST_SIZES[i];
 
-    int testSize = 10;
-int testSeed = BASE_SEED;
+        for (int series = 0; series < SERIES_COUNT; series++) {
+            int seed = BASE_SEED + series;
 
-QueueElement* data = new QueueElement[testSize];
+            measureMaxHeapInsert(file, currentSize, series, seed);
+        }
+    }
 
-generateData(data, testSize, testSeed);
-
-// tymczasowy test - zapisujemy kilka danych do pliku
-for (int i = 0; i < testSize; i++) {
-    file << "TEST,generated,"
-         << testSize << ","
-         << 0 << ","
-         << testSeed << ","
-         << 0 << ","
-         << data[i].value << ","
-         << data[i].priority
-         << "\n";
-}
-
-delete[] data;
-
-file.close();
+    file.close();
     std::cout<<"Utworzono plik results.csv" <<std::endl;
 }
 
@@ -61,4 +52,41 @@ void Research::generateData(QueueElement* data, int size, int seed){
         data[i].value = generateRandomNumber(0, MAX_VALUE);
         data[i].priority = generateRandomNumber(0, maxPriority);
     }
+}
+
+void Research::measureMaxHeapInsert(std::ofstream& file, int size, int series, int seed){
+    QueueElement* data = new QueueElement[size];
+
+    //generujemy dane przed pomiarem
+    generateData(data, size, seed);
+
+    //tworzymy kopie struktury przed pomiarem
+    MaxHeapPriorityQueue* copies = new MaxHeapPriorityQueue[COPIES_COUNT];
+
+    for (int copy = 0;copy < COPIES_COUNT; copy++) {
+        for (int i = 0; i < size; i++) {
+            copies[copy].insert(data[i].value, data[i].priority);
+        }
+    }
+
+    //przygotowanie elementu dodawanaego
+    int newValue = generateRandomNumber(0, MAX_VALUE);
+    int newPriority = generateRandomNumber(0, 10 * size);
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+    for (int copy = 0; copy < COPIES_COUNT; copy++) {
+        copies[copy].insert(newValue, newPriority);
+
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+
+    long long totalTime = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+    double avgTime = static_cast<double>(totalTime) / COPIES_COUNT;
+
+    file << "MaxHeap,insert,"<<size<<","<<series<<","<<seed<<","<<COPIES_COUNT<<","<<totalTime<<","<<avgTime<<"\n";
+
+    delete[] copies;
+    delete[] data;
 }
--- Research.h
+++ Research.h
@@ -8,6 +8,8 @@ class Research {
         int generateRandomNumber(int minValue, int maxValue);
         
         void generateData(QueueElement* data, int size, int seed); //przygotowywuje dane do badan
+
+        void measureMaxHeapInsert(std::ofstream& file, int size, int series, int seed); //pomiar insert kopca
     public:
         void runAll(); //uruchamia wszystkie badania i zapisuje wyniki do csv
 
